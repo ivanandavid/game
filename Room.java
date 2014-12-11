f35dcb4ce85123effcf1c85a55a262735e50c5d0
davester14 <davester14@users.noreply.github.com>
2014-12-11 13:42:04 -0500

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,46 @@
+//package AdventureGame;
+
+import java.util.*;
+public class Room{
+	
+	private int roomID;
+	private String title;
+	private String description;
+	private String long_desc;
+	private int N,E,S,W; //add U, D if needed
+
+	//constructos
+	Room(int id, String title, String description){
+		roomID = id;
+		this.title = title;
+		this.description = description;
+	}
+	
+	void setExits(int N, int E, int S, int W) {
+		this.N = N;
+		this.E = E;
+		this.S = S;
+		this.W = W;		
+	}
+	
+	int getExit(char c) {
+		switch (c) {
+		case 'N': return this.N;
+		case 'E': return this.E;
+		case 'S': return this.S;
+		case 'W': return this.W;
+		default: return 0;
+		}
+	}
+	
+	int getRoomID()	  { return roomID; }
+	String getTitle() { return title; }
+	String getDesc()  { return description; }
+	
+	//most classes need to have this
+	public String toString(){
+		String s = String.format("Title=%-25s\tDescription=%s",title,description);		
+		return s;
+	}
+
+}
