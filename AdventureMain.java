2f266bc3011a41104d6106079aeca45da4334738
cowking232 <ivandenomme@hotmail.com>
2014-12-12 14:17:53 -0500

    Update AdventureMain.java

--- AdventureMain.java
+++ AdventureMain.java
@@ -121,82 +121,65 @@ public class AdventureMain {
 		Room r = new Room(1, "Jail Cell", "");
 		//		   N E S W
 		r.setExits(0,2,0,0);
-		r.setDesc(
-		"A small and dark jail cell. Your sanity feels unstable in this room.");
+		r.setDesc("A small and dark jail cell. Your sanity feels unstable in this room.");
 		roomList.add(r);
 		
 		r = new Room(2, "Jailblock", "It's dark.");
 		//		   N E S W
-		r.setExits(0,3,0,1); //fix these exits
-		r.setDesc(
-		"A massive gloomy room. You feel uneasy on the outside of your cell");// Description goes here ivan
+		r.setExits(0,3,0,1); 
+		r.setDesc("A massive gloomy room. You feel uneasy on the outside of your cell");
 		roomList.add(r);
 		
 		r = new Room(3, "Hub", "");
 		//		   N E S W
-		r.setExits(4,7,6,2); //fix these exits
-		r.setDesc(
-		"This appears to have entrances to many other rooms. Dont get lost, or its game over man!");// Description goes here ivan
+		r.setExits(4,7,6,2); 
+		r.setDesc("This appears to have entrances to many other rooms. Dont get lost, or its game over man!");
 		roomList.add(r);
 		
 		// *******************************Requires flash light because it is so dark************************************8
 		r = new Room(4, "cafe", "");
 		//		   N E S W
-		r.setExits(0,9,3,5); //fix these exits
-		r.setDesc(
-		"Its the cafateria, but its way too dark to see anything");// needs object for progression
+		r.setExits(0,9,3,5); 
+		r.setDesc("Its the cafateria, but its way too dark to see anything");// needs object for progression
 		roomList.add(r);
 		
 		r = new Room(5, "kitchen", "");
 		//		   N E S W
-		r.setExits(0,4,0,0); //fix these exits
-		r.setDesc(
-		"It is surprisingly clean inside the kitchen");// after cafe
+		r.setExits(0,4,0,0); 
+		r.setDesc("It is surprisingly clean inside the kitchen");// after cafe
 		roomList.add(r);
 		
 		r = new Room(6, "reception", "");
 		//		   N E S W
-		r.setExits(3,0,0,0); //fix these exits
-		r.setDesc(
-		"You feel jittery being this close to the exit. There is a guard with his back turned to you");// Description goes here ivan
+		r.setExits(3,10,0,0); //** you can only go east if you have an object ***
+		r.setDesc("You feel jittery being this close to the exit. There is a guard with his back turned to you");
 		roomList.add(r);
 		
 		r = new Room(7, "barracks", "");
 		//		   N E S W
-		r.setExits(9,8,0,3); //fix these exits
-		r.setDesc(
-		"Where are all the guards? There is a large mound of weapons on the floor");// Description goes here ivan
+		r.setExits(9,8,0,3); 
+		r.setDesc("Where are all the guards? There is a large mound of weapons on the floor");
 		roomList.add(r);
 		
 		r = new Room(8, "filing room", "");
 		//		   N E S W
-		r.setExits(0,0,0,7); //fix these exits
-		r.setDesc(
-		"Filing cabinets take up most of the space in this small room");// Description goes here ivan
+		r.setExits(0,0,0,7); 
+		r.setDesc("Filing cabinets take up most of the space in this small room");
 		roomList.add(r);
 		
 		r = new Room(9, "warden's office", "");
 		//		   N E S W
-		r.setExits(0,0,7,0); //fix these exits
-		r.setDesc(
-		"You are so close to freedom. this place emanates with evil");// Description goes here ivan
+		r.setExits(0,0,7,0); 
+		r.setDesc("You are so close to freedom. this place emanates with evil");
 		roomList.add(r);
 		
 		// *******************************needs an object to enter***********************************************
-		r = new Room(9, "vault", "");
+		r = new Room(10, "vault", "");
 		//		   N E S W
-		r.setExits(0,0,7,0); //fix these exits
-		r.setDesc(
-		"There is no treasure! Nothing but a small device");// Description goes here ivan
+		r.setExits(0,0,0,0); 
+		r.setDesc("There is no treasure! Nothing but a small device");
 		roomList.add(r);
-		
-		//JAILBLOCK: You are in disbelief at your luck, as the iron door of your cell silently swings open.
-		//Stepping into the gloom, it encapulates you. Stumbling over a lone chair,  You are able to
-		// make out a door at the far end of the massive room.
-		//HUB This room is refreshingly well lit. You actually have choices here. On the north side, a 
-		//gleaming neon sign states CAFATERIA. The east, the dreaded barracks waits, with gaping double doors. 
-		//finally, to the left is reception, the entrance, and freedom!	
-		
+	
 		
 		//	for (Room m : roomList){
 		//		System.out.println(m.toString());
